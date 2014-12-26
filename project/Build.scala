bba4ea1c2a0409a1b643e9417dc82d1d708ac37e
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2014-12-27 05:10:09 +0530

    Bumping up the version

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import sbt._
 
 object Build extends Build {
 
-  val AppVersion = "0.0.1-SNAPSHOT"
+  val AppVersion = "0.0.2-SNAPSHOT"
   val ScalaVersion = "2.11.1"
 
   lazy val main = Project("predicates", file("."))
