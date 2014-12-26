ff5c5018fc991d97586fb9cb8f04fd8bf47368d5
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2014-12-27 04:32:16 +0530

    Making project spec updates
    * adding SNAPSHOT to the version
    * adding publishTo info
    * adding pomExtra with dev details
    * Enabling parallel execution
    * Making default scala version as 2.11.1

--- project/Build.scala
+++ project/Build.scala
@@ -3,30 +3,58 @@ import sbt._
 
 object Build extends Build {
 
-  val AppVersion = "0.0.1"
-  val ScalaVersion = "2.10.4"
+  val AppVersion = "0.0.1-SNAPSHOT"
+  val ScalaVersion = "2.11.1"
 
-  val main = Project("predicates", file("."))
+  lazy val main = Project("predicates", file("."))
     .settings(organization := "in.ashwanthkumar",
       version := AppVersion,
       libraryDependencies += (
         if (scalaVersion.value.startsWith("2.1")) "org.scalatest" %% "scalatest" % "2.1.3" % "test"
         else "org.scalatest" %% "scalatest" % "1.9.1" % "test"
       ),
-      crossScalaVersions := Seq("2.11.1", "2.10.3", "2.9.1", "2.9.2", "2.9.3")
+      crossScalaVersions := Seq("2.11.1", "2.10.4", "2.9.1", "2.9.2", "2.9.3")
   )
 
   override val settings = super.settings ++ Seq(
     fork in run := false,
-    resolvers += Resolver.sonatypeRepo("snapshots"),
-    parallelExecution in This := false,
+    parallelExecution in This := true,
     publishMavenStyle := true,
     crossPaths := true,
     publishArtifact in Test := false,
     publishArtifact in(Compile, packageDoc) := false,
     // publishing the main sources jar
     publishArtifact in(Compile, packageSrc) := true,
-    credentials += Credentials(Path.userHome / ".ivy2" / ".credentials")
+    credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
+    publishTo <<= version { (v: String) =>
+      val nexus = "https://oss.sonatype.org/"
+      if (v.trim.endsWith("SNAPSHOT"))
+        Some("snapshots" at nexus + "content/repositories/snapshots")
+      else
+        Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+    },
+    pomExtra := _pomExtra
   )
+
+  val _pomExtra =
+    <url>http://github.com/ashwanthkumar/predicates</url>
+      <licenses>
+        <license>
+          <name>Apache License, Version 2.0</name>
+          <url>http://www.apache.org/licenses/LICENSE-2.0.html</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
+      <scm>
+        <url>git@github.com:ashwanthkumar/predicates.git</url>
+        <connection>scm:git:git@github.com:ashwanthkumar/predicates.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>ashwanthkumar</id>
+          <name>Ashwanth Kumar</name>
+          <url>http://www.ashwanthkumar.in/</url>
+        </developer>
+      </developers>
 
 }
